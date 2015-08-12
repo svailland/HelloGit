feed402d06d51e1922a42b16414fa081901f181b
Stephane Vailland <stephane.vailland@st.com>
2015-08-12 16:31:41 +0200

    modification 1 dans la branche de dev
    Conflicts:
    	src/HelloGIT.java

--- src/HelloGIT.java
+++ src/HelloGIT.java
@@ -5,6 +5,7 @@ public class HelloGIT 	{
 	public static void main( String arg[] )	{
 		
 		// modification 1 dans la branche master
+ 		// modification 1 dans la branche de dev
 		System.out.println( "Bonjour GIT monde !!" );
 	}
 }
