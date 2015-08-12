63e8197d28428cc71c926d368de1e7b5e3b48f5d
Stephane Vailland <stephane.vailland@st.com>
2015-08-12 18:02:31 +0200

    Modification 4

--- src/HelloGIT.java
+++ src/HelloGIT.java
@@ -8,6 +8,7 @@ public class HelloGIT 	{
  		// modification 1 dans la branche de dev
 		// modification 2
 		// modification 3
+		// modification 4
 		System.out.println( "Bonjour GIT monde !!" );
 	}
 }
